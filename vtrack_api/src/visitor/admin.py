b599bd055a7ec54b3fdf63d47df1daf628019631
ayushi.agrawal <ayushi.agrawal@acsicorp.com>
2023-04-12 17:17:29 +0530

    Registered Category, Purpose, Timing and Visitor model.

--- vtrack_api/src/visitor/admin.py
+++ vtrack_api/src/visitor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
 
-# Register your models here.
+from visitor.models import Category, Purpose, Timing, Visitor
+
+admin.site.register(Category, GuardedModelAdmin)
+admin.site.register(Purpose, GuardedModelAdmin)
+admin.site.register(Timing, GuardedModelAdmin)
+admin.site.register(Visitor, GuardedModelAdmin)
