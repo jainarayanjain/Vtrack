d072ab7dc427f799c32bbe64e2c64c2e08eb013e
harsh.nohwar <harsh.nohwar@acsicorp.com>
2023-04-12 17:03:39 +0530

    Added Serializers

--- vtrack_api/src/organization/serializers.py
+++ vtrack_api/src/organization/serializers.py
@@ -1,3 +1,30 @@
 from rest_framework import serializers
 
-# Create your serializers here.
+from organization.models import Address, Country, Organization
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    """Country Serializer"""
+
+    class Meta:
+        model = Country
+        fields = '__all__'
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    """Organization Serializer"""
+
+    class Meta:
+        model = Organization
+        fields = '__all__'
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    """Address Serializer"""
+
+    organization = OrganizationSerializer(read_only=True)
+    country = CountrySerializer(read_only=True)
+
+    class Meta:
+        model = Address
+        fields = '__all__'
