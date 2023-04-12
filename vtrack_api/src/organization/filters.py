7fb3f575cfd2407fd8fd91787a8018270b9eb33f
harsh.nohwar <harsh.nohwar@acsicorp.com>
2023-04-12 17:03:24 +0530

    Added Filters

--- vtrack_api/src/organization/filters.py
+++ vtrack_api/src/organization/filters.py
@@ -1,3 +1,27 @@
 from django_filters import rest_framework as filters
 
-# Create your filters here.
+from organization.models import Address, Country, Organization
+
+
+class AddressFilterSet(filters.FilterSet):
+    """Address Filter Set"""
+
+    class Meta:
+        model = Address
+        fields = "__all__"
+
+
+class CountryFilterSet(filters.FilterSet):
+    """Country Filter Set"""
+
+    class Meta:
+        model = Country
+        fields = "__all__"
+
+
+class OrganizationFilterSet(filters.FilterSet):
+    """Address Filter Set"""
+
+    class Meta:
+        model = Organization
+        fields = "__all__"
