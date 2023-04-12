6a28d28128bd2e649fc49a10be9b8b3b45066051
ayushi.agrawal <ayushi.agrawal@acsicorp.com>
2023-04-12 19:00:22 +0530

    Added CategoryFilterSet, PurposeFilterSet, VisitorFilterSet and TimingFilterSet

--- vtrack_api/src/visitor/filters.py
+++ vtrack_api/src/visitor/filters.py
@@ -1,3 +1,35 @@
 from django_filters import rest_framework as filters
 
-# Create your filters here.
+from visitor.models import Category, Purpose, Timing, Visitor
+
+
+class CategoryFilterSet(filters.FilterSet):
+    """Category Filter Set"""
+
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class PurposeFilterSet(filters.FilterSet):
+    """Purpose Filter Set"""
+
+    class Meta:
+        model = Purpose
+        fields = "__all__"
+
+
+class VisitorFilterSet(filters.FilterSet):
+    """Visitor Filter Set"""
+
+    class Meta:
+        model = Visitor
+        fields = "__all__"
+
+
+class TimingFilterSet(filters.FilterSet):
+    """Timing Filter Set"""
+
+    class Meta:
+        model = Timing
+        fields = "__all__"
