48c13dd81820191158c2acae8fb0642359d40c86
ayushi.agrawal <ayushi.agrawal@acsicorp.com>
2023-04-12 19:01:28 +0530

    Added CategorySerializer, NationalIdentitySerializer, VisitorTypeSerializer, PurposeSerializer, VisitorSerializer and TimingSerializer

--- vtrack_api/src/visitor/serializers.py
+++ vtrack_api/src/visitor/serializers.py
@@ -1,3 +1,66 @@
 from rest_framework import serializers
 
-# Create your serializers here.
+from user.serializers import UserSerializer
+from visitor.models import (
+    Category,
+    NationalIdentity,
+    Purpose,
+    Timing,
+    Visitor,
+    VisitorType,
+)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Category Serializer"""
+
+    class Meta:
+        model = Category
+        exclude = ["type"]
+
+
+class NationalIdentitySerializer(CategorySerializer):
+    """National Identity Serializer"""
+
+    class Meta(CategorySerializer.Meta):
+        model = NationalIdentity
+
+
+class VisitorTypeSerializer(CategorySerializer):
+    """Visitor Type Serializer"""
+
+    class Meta(CategorySerializer.Meta):
+        model = VisitorType
+
+
+class PurposeSerializer(serializers.ModelSerializer):
+    """Purpose Serializer"""
+
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Purpose
+        fields = "__all__"
+
+
+class VisitorSerializer(serializers.ModelSerializer):
+    """Visitor Serializer"""
+
+    purpose = PurposeSerializer(read_only=True)
+    visitor_type = VisitorTypeSerializer(read_only=True)
+    nid_type = NationalIdentitySerializer(read_only=True)
+    contact_person = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Visitor
+        fields = "__all__"
+
+
+class TimingSerializer(serializers.ModelSerializer):
+    """Timing Serializer"""
+
+    visitor = VisitorSerializer(read_only=True)
+
+    class Meta:
+        model = Timing
+        fields = "__all__"
