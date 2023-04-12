ddc6efaf1539bb712fa9013389d0ab45bd9df37f
harsh.nohwar <harsh.nohwar@acsicorp.com>
2023-04-12 17:03:00 +0530

    Removed useless comments

--- vtrack_api/src/organization/admin.py
+++ vtrack_api/src/organization/admin.py
@@ -5,3 +5,4 @@ from organization.models import Address, Country, Organization
 admin.site.register(Address, GuardedModelAdmin)
 admin.site.register(Country, GuardedModelAdmin)
 admin.site.register(Organization, GuardedModelAdmin)
+
