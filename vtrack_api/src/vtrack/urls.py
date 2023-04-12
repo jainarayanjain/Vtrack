255e877acf29e3c37c14feace8e0942f6c33af81
Amogh Madan <amogh.madan@acsicorp.com>
2023-04-12 15:40:59 +0530

    Add swagger urls.

--- vtrack_api/src/vtrack/urls.py
+++ vtrack_api/src/vtrack/urls.py
@@ -14,9 +14,62 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.utils.translation import gettext_lazy
+
+admin.site.site_title = gettext_lazy("vTrack admin")
+admin.site.site_header = gettext_lazy("vTrack administration")
+admin.site.index_title = gettext_lazy("vTrack administration")
+
+v1 = []
+
+apis = [
+    path("v1/", include(v1)),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(apis)),
 ]
+
+if settings.DEBUG:  # pragma: no cover
+    from django.conf.urls.static import static
+    from django.urls import re_path
+    from drf_yasg import openapi
+    from drf_yasg.views import get_schema_view
+    from rest_framework import permissions
+
+    info = openapi.Info(
+        title="Purchase Order API",
+        default_version="v1",
+        description="Purchase order generation",
+        terms_of_service="https://acsicorp.com/",
+        contact=openapi.Contact(email="aakarsh.singh@acsicorp.com"),
+        license=openapi.License(name="Proprietary License"),
+    )
+
+    SchemaView = get_schema_view(
+        info, public=True, permission_classes=[permissions.AllowAny]
+    )
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(
+            r"^swagger(?P<format>\.json|\.yaml)$",
+            SchemaView.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        re_path(
+            r"^swagger/$",
+            SchemaView.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        re_path(
+            r"^redoc/$",
+            SchemaView.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
+    ]
