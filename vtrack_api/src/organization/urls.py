641625fd18e0f455fabdfda17883504aad3a7b6c
harsh.nohwar <harsh.nohwar@acsicorp.com>
2023-04-13 13:43:09 +0530

    Mapped all views to urls

--- vtrack_api/src/organization/urls.py
+++ vtrack_api/src/organization/urls.py
@@ -1,3 +1,11 @@
-from django.urls import path
+from rest_framework import routers
+
+from organization.views import AddressViewSet, CountryViewSet, OrganizationViewSet
+
+router = routers.DefaultRouter()
+router.register(r"addresses", AddressViewSet, basename="address")
+router.register(r"countries", CountryViewSet, basename="country")
+router.register(r"organizations", OrganizationViewSet, basename="organization")
 
 urlpatterns = []
+urlpatterns += router.urls
