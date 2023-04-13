3d435bc215581f7119c875cea724bf6c7dcfae7a
harsh.nohwar <harsh.nohwar@acsicorp.com>
2023-04-13 12:14:31 +0530

    Added Views

--- vtrack_api/src/organization/views.py
+++ vtrack_api/src/organization/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from organization.models import Address, Country, Organization
+from organization.serializers import (
+    AddressSerializer,
+    CountrySerializer,
+    OrganizationSerializer,
+)
+from organization.filters import (
+    AddressFilterSet,
+    CountryFilterSet,
+    OrganizationFilterSet,
+)
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    """Address View Set"""
+
+    queryset = Address.objects.filter(is_active=True)
+    serializer_class = AddressSerializer
+    filterset_class = AddressFilterSet
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    """Country View Set"""
+
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    filterset_class = CountryFilterSet
+
+
+class OrganizationViewSet(viewsets.ModelViewSet):
+    """Organization View Set"""
+
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+    filterset_class = OrganizationFilterSet
