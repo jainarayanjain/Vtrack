8fe7fdb743368e9ba5c16bb7af87776e1937f912
ayushi.agrawal <ayushi.agrawal@acsicorp.com>
2023-07-14 14:52:29 +0530

    Added filters for user.

--- vtrack_api/src/user/filters.py
+++ vtrack_api/src/user/filters.py
@@ -1,6 +1,22 @@
 from django_filters import rest_framework as filters
 
-from user.models import Department, Profile
+from user.models import Address, Country, Department, Organization, Profile
+
+
+class AddressFilterSet(filters.FilterSet):
+    """Address Filter Set"""
+
+    class Meta:
+        model = Address
+        fields = "__all__"
+
+
+class CountryFilterSet(filters.FilterSet):
+    """Country Filter Set"""
+
+    class Meta:
+        model = Country
+        fields = "__all__"
 
 
 class DepartmentFilterSet(filters.FilterSet):
@@ -8,6 +24,14 @@ class DepartmentFilterSet(filters.FilterSet):
 
     class Meta:
         model = Department
+        fields = "__all__"
+
+
+class OrganizationFilterSet(filters.FilterSet):
+    """Address Filter Set"""
+
+    class Meta:
+        model = Organization
         fields = "__all__"
 
 
