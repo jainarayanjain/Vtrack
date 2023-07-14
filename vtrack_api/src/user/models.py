44e74369862a7d741bb585ed72b3f6acfb2161f4
ayushi.agrawal <ayushi.agrawal@acsicorp.com>
2023-07-14 14:51:48 +0530

    Added models for user.

--- vtrack_api/src/user/models.py
+++ vtrack_api/src/user/models.py
@@ -2,7 +2,77 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy
 
-from organization.models import Address
+
+class Country(models.Model):
+    """Country Model"""
+
+    name = models.CharField(gettext_lazy("name"), max_length=50)
+    code = models.CharField(gettext_lazy("code"), max_length=10)
+    created = models.DateTimeField(gettext_lazy("created"), auto_now_add=True)
+    updated = models.DateTimeField(gettext_lazy("updated"), auto_now=True)
+
+    class Meta:
+        verbose_name = gettext_lazy("country")
+        verbose_name_plural = gettext_lazy("countries")
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    """Organization Model"""
+
+    name = models.CharField(gettext_lazy("name"), max_length=100)
+    is_active = models.BooleanField(gettext_lazy("active"), default=True)
+    created = models.DateTimeField(gettext_lazy("created"), auto_now_add=True)
+    updated = models.DateTimeField(gettext_lazy("updated"), auto_now=True)
+
+    class Meta:
+        verbose_name = gettext_lazy("organization")
+        verbose_name_plural = gettext_lazy("organizations")
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    """Address Model"""
+
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        verbose_name=gettext_lazy("organization"),
+    )
+    street = models.CharField(gettext_lazy("street"), max_length=256)
+    city = models.CharField(gettext_lazy("city"), max_length=50)
+    region = models.CharField(gettext_lazy("region"), max_length=50)
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE, verbose_name=gettext_lazy("country")
+    )
+    postcode = models.BigIntegerField(gettext_lazy("postcode"))
+    code = models.CharField(gettext_lazy("code"), max_length=20)
+    is_active = models.BooleanField(gettext_lazy("active"), default=True)
+
+    class Meta:
+        verbose_name = gettext_lazy("address")
+        verbose_name_plural = gettext_lazy("addresses")
+
+    def __str__(self):  # pragma: no cover
+        address = (
+            ", ".join([self.street, self.city, self.region, str(self.country)])
+            + " - "
+            + str(self.postcode)
+        )
+        return address
+
+    @staticmethod
+    def clean_location(location):
+        return location.strip().lower().title()
+
+    def save(self, *args, **kwargs):
+        self.city = self.clean_location(self.city)
+        self.region = self.clean_location(self.region)
+        super().save(*args, **kwargs)
 
 
 class Department(models.Model):
@@ -29,6 +99,7 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         verbose_name=gettext_lazy("user"),
     )
+    employee_id = models.IntegerField(gettext_lazy("employee id"), unique=True)
     phone = models.BigIntegerField(gettext_lazy("phone number"))
     department = models.ForeignKey(
         Department,
