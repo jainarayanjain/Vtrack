315d60e3dfbf4a8eeb14d2a7d8a774e5d3d0b78a
ayushi.agrawal <ayushi.agrawal@acsicorp.com>
2023-07-14 14:59:08 +0530

    Added managers.py

--- vtrack_api/src/visitor/managers.py
+++ vtrack_api/src/visitor/managers.py
@@ -1,17 +1,24 @@
 from django.db import models
 
-from visitor.choices import Type
+from visitor.choices import Kind
 
 
 class NationalIdentityManager(models.Manager):
     """National Identity Manager"""
 
     def get_queryset(self):
-        return super().get_queryset().filter(type=Type.NID)
+        return super().get_queryset().filter(kind=Kind.NID)
 
 
-class VisitorTypeManager(models.Manager):
-    """Visitor Type Manager"""
+class CategoryManager(models.Manager):
+    """Category Manager"""
 
     def get_queryset(self):
-        return super().get_queryset().filter(type=Type.VISITOR_TYPE)
+        return super().get_queryset().filter(kind=Kind.CATEGORY)
+
+
+class PurposeManager(models.Manager):
+    """Purpose Manager"""
+
+    def get_queryset(self):
+        return super().get_queryset().filter(kind=Kind.PURPOSE)
