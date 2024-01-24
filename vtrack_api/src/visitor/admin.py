a891c566dd0925141430bbdb83cd92c4f392f799
Amogh Madan <amogh.madan@acsicorp.com>
2024-01-24 18:07:33 +0530

    Added Models in the visitor admin panel

--- vtrack_api/src/visitor/admin.py
+++ vtrack_api/src/visitor/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 
-from visitor.models import Dropdown, Purpose, Timing, Visitor
+from visitor.models import AccessCard, Approval, Category, Host, NIDType, Timing, \
+    Visitor, Valid
 
-admin.site.register(Dropdown, GuardedModelAdmin)
-admin.site.register(Purpose, GuardedModelAdmin)
+admin.site.register(AccessCard, GuardedModelAdmin)
+admin.site.register(Approval, GuardedModelAdmin)
+admin.site.register(Category, GuardedModelAdmin)
+admin.site.register(Host, GuardedModelAdmin)
+admin.site.register(NIDType, GuardedModelAdmin)
 admin.site.register(Timing, GuardedModelAdmin)
+admin.site.register(Valid, GuardedModelAdmin)
 admin.site.register(Visitor, GuardedModelAdmin)
