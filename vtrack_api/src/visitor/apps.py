551f19351c6962bd3751e7fdb7f8b7fe42f09400
Amogh Madan <amogh.madan@acsicorp.com>
2024-01-30 13:22:09 +0530

    Added ready method in app.py

--- vtrack_api/src/visitor/apps.py
+++ vtrack_api/src/visitor/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class VisitorConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "visitor"
+
+    def ready(self):
+        from visitor.signals import visitor_signal
+        from visitor.receivers import visitor_post_save
+
+        visitor_signal.connect(visitor_post_save)
