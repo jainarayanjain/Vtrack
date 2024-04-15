cf125a8c6ee4e833e17f77ca5119fc2063e6bd12
Amogh Madan <amogh.madan@acsicorp.com>
2024-04-15 13:20:11 +0530

    Added Purpose of Visit Configurations

--- vtrack_api/src/visitor/admin.py
+++ vtrack_api/src/visitor/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 
 from visitor.models import AccessCard, Approval, Category, Host, NIDType, Timing, \
-    VisitorDetail, Valid
+    VisitorDetail, Valid, PurposeOfVisit
 
 admin.site.register(AccessCard, GuardedModelAdmin)
 admin.site.register(Approval, GuardedModelAdmin)
 admin.site.register(Category, GuardedModelAdmin)
+admin.site.register(PurposeOfVisit, GuardedModelAdmin)
 admin.site.register(Host, GuardedModelAdmin)
 admin.site.register(NIDType, GuardedModelAdmin)
 admin.site.register(Timing, GuardedModelAdmin)
