4ca7262262d0c3028d86bdc6c874f48fc1ef6f29
Amogh Madan <amogh.madan@acsicorp.com>
2024-08-13 00:59:08 +0530

    Added filter in access card

--- vtrack_api/src/visitor/admin.py
+++ vtrack_api/src/visitor/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 
-from visitor.models import AccessCard, Approval, Category, Host, NIDType, Timing, \
-    VisitorDetail, Valid, PurposeOfVisit
+from visitor.models import AccessCard, Approval, Category, Host, NIDType, \
+    PurposeOfVisit, Timing, VisitorDetail, Valid
+from visitor.utilites import CategoryEnum
 
-admin.site.register(AccessCard, GuardedModelAdmin)
 admin.site.register(Approval, GuardedModelAdmin)
 admin.site.register(Category, GuardedModelAdmin)
 admin.site.register(PurposeOfVisit, GuardedModelAdmin)
@@ -13,3 +13,20 @@ admin.site.register(NIDType, GuardedModelAdmin)
 admin.site.register(Timing, GuardedModelAdmin)
 admin.site.register(Valid, GuardedModelAdmin)
 admin.site.register(VisitorDetail, GuardedModelAdmin)
+
+
+class AccessCardModelAdmin(admin.ModelAdmin):
+    """Access Card Model Admin : for removing extra category"""
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form = super(AccessCardModelAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['category'].queryset = Category.objects.filter(
+            id__in=
+            [CategoryEnum.EMPLOYEE.value,
+             CategoryEnum.INTERVIEW.value,
+             CategoryEnum.VENDOR.value,
+             CategoryEnum.GUEST.value])
+        return form
+
+
+admin.site.register(AccessCard, AccessCardModelAdmin)
---
+++ vtrack_api/src/visitor/utilites.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class CategoryEnum(Enum):
+    """Enum for Category"""
+
+    EMPLOYEE = 1
+    INTERVIEW = 2
+    VENDOR = 3
+    GUEST = 4
+    CLIENT = 5
+    VISITOR = 6
+    NEWHIRES = 7
